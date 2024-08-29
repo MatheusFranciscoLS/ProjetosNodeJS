4c58a5586f805cb4fac1871b62e9f25a5bff488d
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-08-28 21:44:37 -0300

    28/08/24

--- biblioteca-api/app.js
+++ biblioteca-api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const livroRoutes = require("./routes/livroRoutes");
 require("dotenv").config();
 require("./config/database"); // Conectando ao banco de dados
@@ -8,6 +9,9 @@ const app = express();
 
 // Middlewares
 app.use(bodyParser.json());
+
+// Habilitando CORS para permitir que o front-end interaja com a API
+app.use(cors());
 
 // Rotas
 app.use("/livros", livroRoutes);
